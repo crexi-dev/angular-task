3363e87a661244fb0374266f10214f75a2e80f81
Ricky Crawford <rickycrawford@Rickys-MBP.attlocal.net>
2021-07-08 15:02:18 -0700

    service: brought in the api

--- src/app/features/profile/store/profile.service.ts
+++ src/app/features/profile/store/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserProfile } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private url = 'https://randomuser.me/api/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getProfiles(): Observable<UserProfile[]> {
+    return this.http.get<UserProfile[]>(`${this.url}/profiles`);
+  }
 }
