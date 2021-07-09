bf052b49f9b1b9fbe0cbaa856b57cd87e59e9171
Ricky Crawford <rickycrawford@Rickys-MBP.attlocal.net>
2021-07-08 17:33:05 -0700

    effects: fixed error by editing misspellings

--- src/app/features/profile/store/profile.actions.ts
+++ src/app/features/profile/store/profile.actions.ts
@@ -1,8 +1,8 @@
-import { createAction, props } from '@ngrx/store';
-import {UserProfile} from '../interfaces'
+import { createAction } from '@ngrx/store';
+//import {UserProfile} from '../interfaces'
 
- const initProfile = createAction('[Profile] Init Profile');
- const initProfileSuccess = createAction('[Profile] Init Profile Success', props<{user: UserProfile}>());
+ const initProfile = createAction('[Profile] Init Profile ');
+ const initProfileSuccess = createAction('[Profile]  Init Profile Success',);
  
 
  export const profileActions = { initProfile, initProfileSuccess };
--- src/app/features/profile/store/profile.effects.ts
+++ src/app/features/profile/store/profile.effects.ts
@@ -2,38 +2,25 @@ import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
 import {ProfileService} from './profile.service'
 import {catchError, map, mergeMap} from 'rxjs/operators';
-import {profileActions} from './profile.actions'
+//import {profileActions} from './profile.actions'
 import { EMPTY } from 'rxjs';
 
 @Injectable()
 export class ProfileEffects {
   loadProfiles$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(profileActions.initProfile),
+      ofType('[Profile] Init'),
       mergeMap(() => this.profileService.getProfiles().pipe(
-        map((res: { results: [[]] }) => {
-            const data: any = res.results[0];
-            const user = {
-                cellNumber: data.cell,
-                city: data.location.city,
-                dateOfBirth: data.dob.date,
-                email: data.email,
-                firstName: data.name.first,
-                lastName: data.name.last,
-                phoneNumber: data.phone,
-                picture: data.picture.medium,
-                state: data.location.state,
-            };
-
-            return profileActions.initProfileSuccess({ user });
-            
-        }),
+        map((profile: any) => ({
+            type: '[profile] Init Profile Success', payload: profile
+        })),
         catchError(() => EMPTY)
+      ))
     )
-)
-);
+  );
         
 
   constructor(private actions$: Actions, private profileService: ProfileService) {
 }
 }
+
