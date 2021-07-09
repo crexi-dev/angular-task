97ad908aa132336369833799bbdddf89f8c00ed0
Ricky Crawford <rickycrawford@Rickys-MBP.attlocal.net>
2021-07-08 17:21:24 -0700

    service: took out observable

--- src/app/features/profile/store/profile.service.ts
+++ src/app/features/profile/store/profile.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs';
 //import { UserProfile } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private url = 'https://randomuser.me/api/'
+   //url = 'https://randomuser.me/api/'
   constructor(private http: HttpClient) { }
 
-  getProfiles(): Observable<any> {
-    return this.http.get<any>(this.url);
+  getProfiles(){
+    return this.http.get('https://randomuser.me/api/');
   }
 }
