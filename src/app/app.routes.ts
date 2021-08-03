db1b2fca7f3c759a57450fc2d3af83b0035135f2
lanana-interview <elanna.grossman+interview@gmail.com>
2021-08-03 02:14:45 -0700

    feat( app): wire up `ProfileListComponent` route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from '@core/layout/page-not-found';
 import { HomePageComponent } from '@features/home-page';
 import { ProfileDetailComponent } from '@features/profile/profile-detail';
+import { ProfileListContainerComponent } from '@features/profile/profile-list';
 
 export const appRoutes: Routes = [
+    {
+        component: ProfileListContainerComponent,
+        data: { name: 'profileList' },
+        path: 'profile-list'
+    },
     {
         component: ProfileDetailComponent,
         data: { name: 'profileDetail' },
