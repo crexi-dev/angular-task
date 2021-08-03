aaf60ec0b5f5a7322b88f16e813602a535b65f27
lanana-interview <elanna.grossman+interview@gmail.com>
2021-08-03 02:06:00 -0700

    chore(profile): add required material dependencies

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,9 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { EffectsModule } from '@ngrx/effects';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { StoreModule } from '@ngrx/store';
 
 import { getProfileReducer } from './store/profile.reducers';
@@ -25,9 +29,13 @@ import { ProfileEffects } from './store/profile.effects';
     imports: [
         CommonModule,
         LayoutModule,
+        MatButtonModule,
         MatCardModule,
         MatDividerModule,
+        MatIconModule,
         MatListModule,
+        MatTableModule,
+        MatTooltipModule,
         EffectsModule.forFeature([ProfileEffects]),
         StoreModule.forFeature('profile', getProfileReducer)
     ]
