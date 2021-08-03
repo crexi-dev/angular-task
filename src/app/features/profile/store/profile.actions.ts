06ba85d2abb2e358c8219bd7ceddfdc20bf4d5d3
lanana-interview <elanna.grossman+interview@gmail.com>
2021-08-03 01:51:59 -0700

    feat(store): add profile actions

--- src/app/features/profile/store/profile.actions.ts
+++ src/app/features/profile/store/profile.actions.ts
@@ -1,5 +1,13 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+
+import { UserProfile } from '@interfaces';
 
 const initProfile = createAction('[Profile] Init');
+const loadProfileSuccess = createAction('[Profile] Load Profile Success', props<{ user: UserProfile }>());
+const loadRandomProfile = createAction('[Profile] Load Random Profile');
 
-export const profileActions = { initProfile };
+export const profileActions = {
+    initProfile,
+    loadProfileSuccess,
+    loadRandomProfile
+};
