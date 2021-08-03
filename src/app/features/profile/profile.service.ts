b8645d9ad2e4a3fb2a9e30ac916da04cc11d3c6e
lanana-interview <elanna.grossman+interview@gmail.com>
2021-08-03 01:46:57 -0700

    feat(profile): add `ProfileService`

---
+++ src/app/features/profile/profile.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+import { IProfile, IProfileDTO } from './interfaces/profile-dto';
+import { UserProfile } from '@interfaces';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProfileService {
+
+    private url: string = `https://randomuser.me/api/`;
+
+    /**
+     * Map DTO from external API to client model.
+     */
+    private mapProfileDtoToUserProfile (profileDTOList: IProfile[]): UserProfile[] {
+
+        const profileList: UserProfile[] = [];
+        profileDTOList.forEach((profileDTO) => {
+
+            const profile: UserProfile = {
+                cellNumber: profileDTO.cell,
+                city: profileDTO.location.city,
+                dateOfBirth: profileDTO.dob.date,
+                email: profileDTO.email,
+                firstName: profileDTO.name.first,
+                id: profileDTO.login.uuid,
+                lastName: profileDTO.name.last,
+                phoneNumber: profileDTO.phone,
+                picture: profileDTO.picture.large,
+                state: profileDTO.location.state
+            };
+            profileList.push(profile);
+
+        });
+
+        return profileList;
+
+    }
+
+    constructor (private httpClient: HttpClient) { }
+
+    /**
+     * Get a randomized profile. Use `shareReplay` to turn cold observable hot.
+     */
+    public getProfile (): Observable<UserProfile> {
+
+        return this.httpClient.get<IProfileDTO>(this.url).pipe(
+            map((profileDTO) => {
+
+                const [userProfile] = this.mapProfileDtoToUserProfile(profileDTO.results);
+                return userProfile;
+
+            }),
+            shareReplay()
+        );
+
+    }
+}
