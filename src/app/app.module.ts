7aa7562cd1d053be492fa83002ebe252a266abc0
lanana-interview <elanna.grossman+interview@gmail.com>
2021-08-03 02:35:53 -0700

    chore(app): wire up material animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
 import { FeaturesModule } from '@features/features.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
         AppComponent
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         CoreModule,
         FeaturesModule
