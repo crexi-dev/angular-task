d167c24c293afe5a960ff2d30584a958da0e0655
robertlb4@gmail.com <robertlb4@gmail.com>
2022-05-23 15:51:07 -0700

    install ngrx router store

--- src/app/core/routing/routing.module.ts
+++ src/app/core/routing/routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from '../../app.routes';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
     exports: [
         RouterModule
     ],
     imports: [
-        RouterModule.forRoot(appRoutes, {
-    enableTracing: false,
-    onSameUrlNavigation: 'reload',
-    paramsInheritanceStrategy: 'always',
-    relativeLinkResolution: 'legacy'
-})
+      RouterModule.forRoot(appRoutes, {
+        enableTracing: false,
+        onSameUrlNavigation: 'reload',
+        paramsInheritanceStrategy: 'always',
+        relativeLinkResolution: 'legacy'
+      }),
+      StoreModule.forRoot({router: routerReducer}),
+      StoreRouterConnectingModule.forRoot(),
     ]
 })
 export class RoutingModule {}
