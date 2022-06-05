5babe0f4121604888e446c38bc3ee71d6a928b11
alb <alberto@nestegg.rent>
2022-06-05 11:07:34 -0500

    ng g c profile-list

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -7,10 +7,12 @@ import { LayoutModule } from '@core/layout/layout.module';
 import { StoreModule } from '@ngrx/store';
 import { ProfileDetailComponent } from './profile-detail';
 import { getProfileReducer } from './store/profile.reducers';
+import { ProfileListComponent } from './profile-list/profile-list.component';
 
 @NgModule({
     declarations: [
-        ProfileDetailComponent
+        ProfileDetailComponent,
+        ProfileListComponent
     ],
     exports: [
         ProfileDetailComponent
