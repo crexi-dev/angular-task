34d3f71b864790474272cf2410c6f3c11dc5ad7f
alb <alberto@nestegg.rent>
2022-06-06 16:34:59 -0500

    Better comment

--- src/app/features/profile/interfaces/profile-state.ts
+++ src/app/features/profile/interfaces/profile-state.ts
@@ -3,5 +3,6 @@ import { UserProfile } from './user-profile';
 export interface ProfileState {
     user?: UserProfile;
     users?: UserProfile[];
+    // TODO: If there was enough time I'd plumb this through and tie a material spinner to it.
     isLoading: boolean;
 }
