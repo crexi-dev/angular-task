a893ba74527f3e09138c4ce2043b9b779228a831
jerryorta.dev@gmail.com <jerryorta.dev@gmail.com>
2022-07-17 21:33:01 -0500

    test(profilelistcomponent): add basic tests

--- src/app/features/profile/profile-list/profile-list.component.spec.ts
+++ src/app/features/profile/profile-list/profile-list.component.spec.ts
@@ -4,22 +4,78 @@ import { provideMockStore } from '@ngrx/store/testing';
 import { LayoutModule } from '@core/layout/layout.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatTableModule } from '@angular/material/table';
+import { profilesFeatureKey, UserProfile } from '@interfaces';
+import { routingActions } from '@core/routing/store/routing.actions';
+import { Store } from '@ngrx/store';
+import { MatIconModule } from '@angular/material/icon';
+
+const profile1: UserProfile = {
+    cellNumber: 'I02 D68-7701',
+    city: 'Selkirk',
+    dateOfBirth: 'Feb 28th, 1955',
+    email: 'mia.roy@example.com',
+    firstName: 'Mia',
+    lastName: 'Roy',
+    phoneNumber: 'T12 Z17-4169',
+    picture: 'https://randomuser.me/api/portraits/thumb/women/40.jpg',
+    state: 'British Columbia',
+    username: 'heavyswan118'
+};
+
+const profile2: UserProfile = {
+    cellNumber: 'I02 D68-7788',
+    city: 'Austin',
+    dateOfBirth: 'Jan 28th, 1955',
+    email: 'john.roy@example.com',
+    firstName: 'John',
+    lastName: 'Roy',
+    phoneNumber: '123412412',
+    picture: 'https://randomuser.me/api/portraits/thumb/women/40.jpg',
+    state: 'Texas',
+    username: 'jroy123'
+};
 
 describe('ProfileListComponent', () => {
 
     let component: ProfileListComponent;
     let fixture: ComponentFixture<ProfileListComponent>;
+    let store: Store;
 
     beforeEach(async () => {
 
         await TestBed.configureTestingModule({
             declarations: [ProfileListComponent],
-            imports: [LayoutModule, MatTableModule, RouterTestingModule],
+            imports: [LayoutModule, MatIconModule, MatTableModule, RouterTestingModule],
             providers: [
-                provideMockStore()
+                provideMockStore({
+                    initialState: {
+                        [profilesFeatureKey]: {
+                            entities: {
+                                [profile1.username]: profile1,
+                                [profile2.username]: profile2
+                            },
+                            ids: [profile1.username, profile2.username],
+                            isLoaded: true
+                        },
+                        routing: {
+                            currentRoute: null,
+                            history: [],
+                            inProgress: false,
+                            name: null,
+                            params: {
+                                id: profile1.username
+                            },
+                            prevRoute: null,
+                            queryParams: null,
+                            url: '/'
+                        }
+                    }
+                })
             ]
         })
         .compileComponents();
+
+        store = TestBed.inject(Store);
 
     });
 
@@ -31,9 +87,26 @@ describe('ProfileListComponent', () => {
 
     });
 
-    it('should create', () => {
+    it('should selectAllProfiles', () => {
 
-        expect(component).toBeTruthy();
+        component.ngOnInit();
+        expect(component.dataSource.length).toEqual(2);
+
+    });
+
+    it('should select user profile', () => {
+
+        jest.spyOn(store, 'dispatch');
+
+        component.selectProfile(profile1);
+
+        expect(store.dispatch).toHaveBeenCalledWith(routingActions.go({ url: `profile/${profile1.username}` }));
+
+    });
+
+    it('should set displayColumns', () => {
+
+        expect(component.displayedColumns).toEqual(['firstName', 'lastName', 'email', 'phoneNumber', 'action']);
 
     });
 
