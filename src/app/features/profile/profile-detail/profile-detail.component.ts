bcc0ffaee485c4be08bdb4465788185816852ebf
ericchristenson <eric.christenson@ggcircuit.com>
2022-08-22 06:50:31 -0700

    Refactored PageDetailComponent.

--- src/app/features/profile/profile-detail/profile-detail.component.ts
+++ src/app/features/profile/profile-detail/profile-detail.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { profileActions } from '@store/actions';
-import { AppState } from '@store/reducers';
-import { getUserProfile } from '@store/selectors';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProfileService } from '@features/profile/profile.service';
+import { UserProfile } from '@interfaces';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
-    selector: 'crx-profile-detail',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: ['./profile-detail.component.scss'],
     templateUrl: './profile-detail.component.html'
 })
 export class ProfileDetailComponent implements OnInit {
 
-    user$ = this.store.select(getUserProfile);
+    user$: Observable<UserProfile>;
+    showMissingProfileNotice = false;
 
-    constructor (private store: Store<AppState>) {}
+    constructor (private _activatedRoute: ActivatedRoute, private _profileService: ProfileService) {}
 
     ngOnInit () {
 
-        this.store.dispatch(profileActions.initProfile());
+        this.#getProfileData();
+
+    }
+
+    /**
+     * Retrieve profile data.
+     * If the `id` is not recognized then show an error notification.
+     * @private
+     */
+    #getProfileData () {
+
+        const id = this._activatedRoute.snapshot.paramMap.get('id') || null;
+        this.user$ = this._profileService.getProfile$(id).pipe(map((result) => {
+
+            if (result === false) {
+
+                this.showMissingProfileNotice = true;
+                return {} as UserProfile;
+
+            } else {
+
+                return result as UserProfile;
+
+            }
+
+        }));
 
     }
 
