b82a58c9d3f3407d5adfc3e9600ad6f8375ff28d
ericchristenson <eric.christenson@ggcircuit.com>
2022-08-22 06:39:51 -0700

    Added conditional properties to UserProfile and created `setDateOfBirth()`.

--- src/app/features/profile/interfaces/user-profile.ts
+++ src/app/features/profile/interfaces/user-profile.ts
@@ -1,3 +1,5 @@
+import { format as dateFnsFormat } from 'date-fns';
+
 export interface UserProfile {
     cellNumber: string;
     city: string;
@@ -8,4 +10,16 @@ export interface UserProfile {
     phoneNumber: string;
     picture: string;
     state: string;
+    id?: string;
+    thumbnail?: string;
 }
+
+/**
+ * Formats a date string to "MMM do, yyyy" (Jan 1st, 1966).
+ *
+ * Useful for matching the `UserProfile.dateOfBirth` formatting.
+ *
+ * @param {string} dateString
+ * @returns {string}
+ */
+export const setDateOfBirth = (dateString: string) => dateFnsFormat(new Date(dateString), 'MMM do, yyyy');
