0777f94e7bb5e48c3fd60432b7d196efdad64aa0
smullen <mrhoipoi0320@gmail.com>
2022-09-10 22:41:26 -0400

    added loading and error states to details page

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatListModule } from '@angular/material/list';
 import { LayoutModule } from '@core/layout/layout.module';
 import { StoreModule } from '@ngrx/store';
@@ -26,6 +27,7 @@ import { ProfileEffects } from './store/profile.effects';
         MatCardModule,
         MatDividerModule,
         MatListModule,
+        MatProgressSpinnerModule,
         StoreModule.forFeature('profile', getProfileReducer),
         EffectsModule.forFeature([ProfileEffects])
     ]
